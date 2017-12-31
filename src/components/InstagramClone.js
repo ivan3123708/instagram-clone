8296b5004bb2b86382be051e2e37e4af291c7821
Ivan <ivan3123708@gmail.com>
2018-01-01 00:27:52 +0100

    Regular update

---
+++ src/components/InstagramClone.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class InstagramClone extends React.Component {
+
+  render() {
+    return (
+      <div>
+        <h1>Hello, this is root component!</h1>
+      </div>
+    )
+  }
+}
+
+export default InstagramClone;
---
+++ src/index.js
@@ -0,0 +1,5 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import InstagramClone from './components/InstagramClone';
+
+ReactDOM.render(<InstagramClone />, document.getElementById('app_root'));
